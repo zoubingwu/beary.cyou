2b7b38f03625e03726777ca8feedd8543da52615
zoubingwu <zoubingwu@gmail.com>
2021-06-26 18:00:38 +0800

    preload assets

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-import fullpage from 'fullpage.js'
+import fullpage from 'fullpage.js';
 
-window.onload = function() {
-  new fullpage('#fullpage', {
-    //options here
-    autoScrolling:true,
-    scrollHorizontally: true
+function loadImage(src, target) {
+  return new Promise(function (resolve, reject) {
+    const img = new Image();
+    img.onload = function () {
+      document.querySelector(target).src = this.src;
+      resolve(img);
+    };
+    img.onerror = reject;
+    img.src = src;
   });
 }
+
+function init() {
+  const c = window.console;
+  window.console = null;
+  new fullpage('#app');
+  window.console = c;
+
+  loadImage('./assets/xiong1.gif', '.xiong1');
+  loadImage('./assets/xiong2.gif', '.xiong2');
+  loadImage('./assets/xiong3.gif', '.xiong3');
+  loadImage('./assets/xiong4.gif', '.xiong4');
+  loadImage('./assets/xiong5.gif', '.xiong5');
+}
+
+window.onload = init;
