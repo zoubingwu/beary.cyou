ccf45f5e8cba453879cb257529786143c6161912
zoubingwu <zoubingwu@gmail.com>
2022-12-10 03:55:02 +0800

    fix: cloudflare worker cors

--- cf/index.ts
+++ cf/index.ts
@@ -1,3 +1,44 @@
+// Reference: https://developers.cloudflare.com/workers/examples/cors-header-proxy
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+};
+
+function handleOptions(request: Request) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  let headers = request.headers;
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    let respHeaders = {
+      ...corsHeaders,
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+      'Access-Control-Allow-Headers': request.headers.get(
+        'Access-Control-Request-Headers'
+      ),
+    };
+    return new Response(null, {
+      headers: respHeaders,
+    });
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    });
+  }
+}
+
 export interface Env {
   DB: D1Database;
 }
@@ -18,7 +59,14 @@ const getComments = async (env: Env, page: number, pageSize: number) => {
     .bind(pageSize, (page - 1) * pageSize)
     .all<Comment>();
 
-  return Response.json(results);
+  const res = await env.DB.prepare(
+    `SELECT COUNT(*) as count FROM Comments`
+  ).first<{ count: number }>();
+
+  return Response.json({
+    data: results,
+    total: res.count,
+  });
 };
 
 const postComment = async (env: Env, content: string, nickname: string) => {
@@ -36,24 +84,33 @@ const postComment = async (env: Env, content: string, nickname: string) => {
 
 export default {
   async fetch(request: Request, env: Env) {
-    const { pathname, searchParams } = new URL(request.url);
+    if (request.method === 'OPTIONS') {
+      return handleOptions(request);
+    }
 
+    const { pathname, searchParams } = new URL(request.url);
+    let response = new Response(null, { status: 404 });
     if (request.method === 'GET' && pathname === '/api/comments') {
       const page = Number(searchParams.get('page')) || 1;
       const pageSize = Number(searchParams.get('pageSize')) || 10;
 
-      return await getComments(env, page, pageSize);
-    }
-
-    if (request.method === 'POST' && pathname === '/api/comments') {
+      response = await getComments(env, page, pageSize);
+    } else if (request.method === 'POST' && pathname === '/api/comments') {
       const body = await request.json<PostCommentBody>();
       const { content, by_nickname } = body;
       if (!content || !by_nickname) {
         return new Response(null, { status: 400 });
       }
-      return await postComment(env, body.content, body.by_nickname);
+
+      response = await postComment(env, body.content, body.by_nickname);
     }
 
-    return new Response(null, { status: 404 });
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+    );
+
+    return response;
   },
 };
