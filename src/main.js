7866d738ab663b48ff02b5b54e008f44af214280
zoubingwu <zoubingwu@gmail.com>
2024-07-23 04:23:49 +0800

    remove fullpage.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import fullpage from 'fullpage.js';
 import xiong1 from './assets/xiong1.gif';
 import xiong2 from './assets/xiong2.gif';
 import xiong3 from './assets/xiong3.gif';
@@ -18,91 +17,6 @@ function loadImage(src, target) {
   img.src = src;
 }
 
-function initFullPageEffect() {
-  let scrolling = false;
-  let q = [];
-  let timer;
-  let prevTouchY = [];
-  let fullpageInstance;
-
-  const app = document.querySelector('#app');
-
-  const c = window.console;
-  window.console = null;
-  fullpageInstance = new fullpage('#app', {
-    autoScrolling: false,
-    fitToSection: false,
-    afterLoad: (_, dest) => {
-      scrolling = false;
-      if (dest.isLast) {
-        app.removeEventListener('wheel', handleWheel);
-        app.removeEventListener('touchmove', handleWheel);
-        app.removeEventListener('touchend', touchEnd);
-      }
-    },
-  });
-  window.console = c;
-
-  function moveSection(delta, threshold) {
-    if (delta > threshold) {
-      fullpageInstance.moveSectionDown();
-      return true;
-    } else if (delta < -threshold) {
-      fullpageInstance.moveSectionUp();
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  function handleWheel(e) {
-    if (e.type === 'touchmove' && e.changedTouches.length > 1) {
-      return;
-    }
-
-    if (scrolling) {
-      e.preventDefault();
-      return;
-    }
-
-    e.preventDefault();
-
-    if (e.type === 'wheel') {
-      q.push(e.deltaY);
-    } else if (e.type === 'touchmove') {
-      const clientY = e.changedTouches[0].clientY;
-      if (prevTouchY.length > 0) {
-        const deltaY = prevTouchY[prevTouchY.length - 1] - clientY;
-        q.push(deltaY);
-      }
-      prevTouchY.push(clientY);
-    }
-
-    const delta = q.reduce((a, b) => a + b, 0);
-    const threshold = e.type === 'wheel' ? 400 : 100;
-    const moving = moveSection(delta, threshold);
-
-    clearTimeout(timer);
-    if (moving) {
-      q = [];
-      scrolling = true;
-    } else {
-      timer = setTimeout(() => {
-        q = [];
-      }, 300);
-    }
-  }
-
-  function touchEnd() {
-    q = [];
-    prevTouchY = [];
-  }
-
-  app.addEventListener('wheel', handleWheel);
-  app.addEventListener('touchmove', handleWheel);
-  app.addEventListener('touchend', touchEnd);
-}
-
 function replaceGifs() {
   loadImage(xiong1, '.xiong1');
   loadImage(xiong2, '.xiong2');
@@ -112,7 +26,7 @@ function replaceGifs() {
 }
 
 function init() {
-  initFullPageEffect();
+  // initFullPageEffect();
   replaceGifs();
 
   import('./Comment').then(({ initComment }) => {
